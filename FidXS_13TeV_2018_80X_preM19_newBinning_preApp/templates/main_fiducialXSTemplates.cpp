c6aa42baa2d191f9ffb084999583d6883ec0c160
vmilosev <vukasin.milosevic@cern.ch>
2021-12-20 14:38:36 +0100

    Adding bkg templates maker and Z+X input files

---
+++ FidXS_13TeV_2018_80X_preM19_newBinning_preApp/templates/main_fiducialXSTemplates.cpp
@@ -0,0 +1,25 @@
+#include "fiducialXSTemplates.C"
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char* argv[])
+{
+
+    cout<<"argc "<<argc<<" argv[11] "<<argv[11]<<endl;
+
+	if(argc != 10 && argc != 11 && argc !=12)
+		return 1;
+
+    if(argc == 10)
+        fiducialXSTemplates(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7], argv[8], argv[9]);
+
+    if(argc == 11)
+        fiducialXSTemplates(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7], argv[8], argv[9], argv[10]);
+
+    if(argc == 12)
+        fiducialXSTemplates(argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7], argv[8], argv[9], argv[10], argv[11]);
+
+	return 0;
+}
